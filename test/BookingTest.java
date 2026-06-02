94b5d00b74d99a35fab79ff80cf02d2452364f9e
tausifs-git <tausifrahman2001@gmail.com>
2026-06-02 21:03:18 +0600

    Adding resetSystem method.

--- test/BookingTest.java
+++ test/BookingTest.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BookingTest {
+
+    @BeforeEach
+    void resetSystem() {
+        ParkingSystem ps = ParkingSystem.getInstance();
+
+        ps.setVehicles(new ArrayList<>());
+        ps.setParkingSlots(new ArrayList<>());
+        ps.setBookings(new ArrayList<>());
+        ps.setSYSTEM_WALLET(new Wallet());
+    }
 }
--- test/WalletTest.java
+++ test/WalletTest.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class WalletTest {
     private static final double DELTA = 1e-9;
+
+    @BeforeEach
+    void resetSystem() {
+        ParkingSystem ps = ParkingSystem.getInstance();
+
+        ps.setVehicles(new ArrayList<>());
+        ps.setParkingSlots(new ArrayList<>());
+        ps.setBookings(new ArrayList<>());
+        ps.setSYSTEM_WALLET(new Wallet());
+    }
 
     @Test
     void defaultConstructorStartsWithZeroBalance() {
